95bffaf377b019e17960a6d2ed954e7797616a45
TruLov <tom.linus13@gmail.com>
2017-10-17 17:41:06 +0200

    Update LogPanel.java

--- competition/muehle/view/LogPanel.java
+++ competition/muehle/view/LogPanel.java
@@ -1,58 +1,37 @@
 package competition.muehle.view;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-
 import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
-import javax.swing.JTextPane;
+import javax.swing.JTextArea;
 
-/**
- * LogPanel class
- * 
- * @author trulof
- * @author jbreih
- * @category View
- * 
- */
-public class LogPanel extends Panel {
-
+public class LogPanel extends Panel{
+	
 	private static final long serialVersionUID = 1L;
 
 	static String mName = "LogPanel";
-	JTextPane mTextPane;
-	JScrollPane mScrollPane;
+	JTextArea mLogger;
+	JScrollPane mScroller; 
 	JScrollBar bar;
-
-	/**
-	 * @param widthIn
-	 *            panel-width
-	 * @param heightIn
-	 *            panel-height
-	 */
-	public LogPanel(int widthIn, int heightIn) {
-
-		super(widthIn, heightIn, mName);
-
-		mTextPane = new JTextPane();
-		mTextPane.setEditable(false);
-		mScrollPane = new JScrollPane(mTextPane);
-		
-		mTextPane.setPreferredSize(new Dimension(mWidth-10, mHeight));
-
-		add(mScrollPane, BorderLayout.CENTER);
-	}
+	
 
 	/**
 	 * 
-	 * @param text
-	 *            text to log
+	 * @param widthIn
+	 * @param heightIn
 	 */
-	public void log(String text) {
-		mTextPane.setText(text + "\n");
+	public LogPanel(int widthIn, int heightIn) {
+		super(widthIn, heightIn, mName);
+		mLogger = new JTextArea(5, 47);
+		mLogger.setEditable(false);
 		
-//		mTextPane.insert(text + "\n", 0);
-//		bar = mScroller.getVerticalScrollBar();
-//		bar.setValue(bar.getMinimum());
+		mScroller = new JScrollPane(mLogger);
+		
+		add(mScroller);
+	}
+	
+	public void log(String text){
+		mLogger.insert(text+"\n", 0);
+		bar = mScroller.getVerticalScrollBar();
+		bar.setValue(bar.getMinimum());
 	}
 }
